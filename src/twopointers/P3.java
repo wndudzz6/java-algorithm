5e3714fa4e022467e7930018fae375b70440472f
wndudzz6 <osmzxcvb@naver.com>
2025-03-30 22:34:01 +0900

    sliding window

--- src/twopointers/P3.java
+++ src/twopointers/P3.java
@@ -1,5 +1,32 @@
 package twopointers;
 
-//3. 최대 매출
+import java.util.Scanner;
+
+//3. 최대 매출 ~ 내가 풀었음 Time Limit Exceeded 슬라이딩 윈도우를 쓰자
 public class P3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] sales = new int[n];
+        int[] sum = new int[n-k];
+        for(int i = 0; i < n; i++) {
+            sales[i] = sc.nextInt();
+        }
+        sc.close();
+        for(int i =0; i < n-k ; i++) {
+            for(int j = 0; j < k; j++) {
+                sum[i] += sales[j+i];
+            }
+        }
+
+        int max = 0;
+        for(int i =0; i < sum.length-1; i++) {
+            if(sum[i]>max){
+                max = sum[i];
+            }
+        }
+        System.out.println(max);
+
+    }
 }
---
+++ src/twopointers/P3GPT.java
@@ -0,0 +1,30 @@
+package twopointers;
+
+import java.util.Scanner;
+
+public class P3GPT {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] sales = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            sales[i] = sc.nextInt();
+        }
+
+        int maxSum = 0;
+        int windowSum = 0;
+
+        for(int i = 0; i < n; i++) {
+            windowSum += sales[i];
+        }
+        maxSum = windowSum;
+
+        for(int i = 0; i < n; i++) {
+            windowSum += sales[i] - sales[i-k];
+            maxSum = Math.max(maxSum, windowSum);
+        }
+        System.out.println(maxSum);
+    }
+}
