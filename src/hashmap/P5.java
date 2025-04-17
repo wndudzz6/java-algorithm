924e7fec9a184112898323caa670c7fe7b74c508
wndudzz6 <osmzxcvb@naver.com>
2025-04-17 19:25:55 +0900

    treeSet으로 k번째 큰 수

--- src/hashmap/P5.java
+++ src/hashmap/P5.java
@@ -1,8 +1,8 @@
 package hashmap;
 
-import java.util.Scanner;
+import java.util.*;
 
-//5. k번쨰 큰 수
+//5. k번쨰 큰 수 ~ hashMap이 아닌 treeSet 사용 중복 제거, 오름차순 정렬
 public class P5 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -16,9 +16,22 @@ public class P5 {
         System.out.println(solution(n, k, arr));
     }
 
+    //카드 중 세 장을 뽑아서 합을 구한다. 이 합 중에서 k번째로 큰 수를 구함
     static int solution(int n, int k, int[] arr) {
-        int answer = 0;
-
-        return answer;
+        TreeSet<Integer> set = new TreeSet<>(Collections.reverseOrder()); // 중복 제거 + 내림차순 정렬
+        for (int i = 0; i<n; i++){
+            for(int j = i+1; j<n; j++){
+                for(int l = j+1; l<n; l++){
+                    int sum =arr[i]+arr[j]+arr[l];
+                    set.add(sum);
+                }
+            }
+        }
+        int count = 0;
+        for(int answer: set){
+            count ++;
+            if(count == k) return answer;
+        }
+        return -1;
     }
 }
