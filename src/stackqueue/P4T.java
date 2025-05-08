205c57c02e7fa248307847d99532d9ef365ed23e
wndudzz6 <osmzxcvb@naver.com>
2025-05-08 23:59:40 +0900

    stack

---
+++ src/stackqueue/P4T.java
@@ -0,0 +1,13 @@
+package stackqueue;
+
+import java.util.Stack;
+
+public class P4T {
+    static int solution(String str){
+        int answer = 0;
+        Stack<Integer> stack = new Stack<>();
+        for(char x : str.toCharArray()){
+
+        }
+    }
+}
